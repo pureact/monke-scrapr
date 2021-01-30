73d61ebfc67adf63dbf2518f1d50b8695d0e8765
Matthew <matthewjo30@gmail.com>
2021-01-30 18:58:36 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import CreateReddit from './components/pages/CreateReddit';
+import CreateTwitter from './components/pages/CreateTwitter';
+import CreateWebsite from './components/pages/CreateWebsite';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard></Dashboard>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={Dashboard}/>
+        <Route path="/create/reddit" exact component={CreateReddit}/>
+        <Route path="/create/twitter" exact component={CreateTwitter}/>
+        <Route path="/create/website" exact component={CreateWebsite}/>
+      </div>
+    </Router>
   );
 }
 
