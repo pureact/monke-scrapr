125f36dd80c38715d8b5994bcb86087392dbbea7
Matthew <matthewjo30@gmail.com>
2021-01-30 17:06:29 -0500

    changing to jsx + some changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import PeopleIcon from '@material-ui/icons/People';
-import AddIcon from '@material-ui/icons/Add';
-import BuildIcon from '@material-ui/icons/Build';
-import HomeIcon from '@material-ui/icons/Home';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Navbar from './Navbar';
 
 const drawerWidth = 240;
 
@@ -30,16 +21,6 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: drawerWidth,
     background: "#49392C"
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    background: "#FFF9C2"
-  },
-  // necessary for content to be below app bar FFF7AE A38266
-  toolbar: theme.mixins.toolbar,
   
 
   appBarButtons: {
@@ -87,57 +68,19 @@ export default function Dashboard() {
         </Toolbar>
       </AppBar>
 
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {['Home', 'Scraprs', 'Create Scraprs'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index === 0 ? <HomeIcon /> : index === 1 ? <BuildIcon /> : <AddIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['User'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index === 0 ? <PeopleIcon /> : <PeopleIcon /> }</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+      <Navbar />
 
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
+            {/* Generate cards based on all configs */}
             <Grid item xs={3} md={3} lg={4}>
               <Paper elevation={5} className={classes.paper}>
                 Test
               </Paper>
             </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={4}>
-              <Paper elevation={5} className={classes.paper}>
-                Test
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={4}>
-              <Paper elevation={5} className={classes.paper}>
-                Test
-              </Paper>
-            </Grid>
+            
           </Grid>
         </Container>
       </main>
