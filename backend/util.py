e9c3711fe33ec6214d21c6a1eb594d335031205e
NAEK2K <naek2k@outlook.com>
2021-01-30 16:53:06 -0500

    added param to util

--- backend/util.py
+++ backend/util.py
@@ -3,8 +3,8 @@ import json
 from uuid import uuid4
 
 
-def generate_config(config_name, **params):
-    file_path = "{}_{}.json".format(uuid4().hex, config_name)
+def generate_config(config_name, directory=".", **params):
+    file_path = "{}/{}_{}.json".format(directory, uuid4().hex, config_name)
     with open(file_path, "w+") as f:
         json.dump(params, f)
     return file_path
