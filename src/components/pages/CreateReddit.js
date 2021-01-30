1865951ebeb3c0ddfd12499ae2759c49db0398b0
Adrian <adrianalexander17@gmail.com>
2021-01-30 15:42:38 -0500

    updated gitignore

--- src/components/pages/CreateReddit.js
+++ src/components/pages/CreateReddit.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -9,6 +8,9 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Copyright from '../Copyright';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Navbar from '../Navbar';
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -23,7 +25,10 @@ const useStyles = makeStyles((theme) => ({
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
-    }
+    },
+    title: {
+        margin: 'auto'
+    },
 }));
 
 export default function CreateReddit() {
@@ -39,95 +44,103 @@ export default function CreateReddit() {
 
     const classes = useStyles();
     return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <AppBar position="static">
-                </AppBar>
+        <div>
+            <Navbar />
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Create Reddit Config
+                        </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <AppBar position="static">
+                    </AppBar>
 
-                <form className="classes.form" noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="configName"
-                        label="Config Name"
-                        name="configName"
-                        autoComplete="configName"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="subreddit"
-                        label="Subreddit"
-                        name="subreddit"
-                        autoComplete="feed"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="numPost"
-                        label="Number of posts to search"
-                        name="numPost"
-                        autoComplete="Number of posts to search"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        fullWidth
-                        id="trackedUsers"
-                        label="Tracked Users"
-                        name="trackedUsers"
-                        autoComplete="trackedUsers"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="keywords"
-                        label="Keywords"
-                        name="keywords"
-                        autoComplete="keywords"
-                        autoFocus
-                    />
-                    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>Choose a filter</Button>
-                    <Menu
-                        id="filterMenu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        <MenuItem onclick={handleClose} id="hot">hot</MenuItem>
-                        <MenuItem onclick={handleClose} id="new">new</MenuItem>
-                        <MenuItem onclick={handleClose} id="rising">rising</MenuItem>
-                        <MenuItem onclick={handleClose} id="controversial">controversial</MenuItem>
-                        <MenuItem onclick={handleClose} id="top">top</MenuItem>
-                    </Menu>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Create config
+                    <form className="classes.form" noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="configName"
+                            label="Config Name"
+                            name="configName"
+                            autoComplete="configName"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="subreddit"
+                            label="Subreddit"
+                            name="subreddit"
+                            autoComplete="feed"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="numPost"
+                            label="Number of posts to search"
+                            name="numPost"
+                            autoComplete="Number of posts to search"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            id="trackedUsers"
+                            label="Tracked Users"
+                            name="trackedUsers"
+                            autoComplete="trackedUsers"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="keywords"
+                            label="Keywords"
+                            name="keywords"
+                            autoComplete="keywords"
+                            autoFocus
+                        />
+                        <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>Choose a filter</Button>
+                        <Menu
+                            id="filterMenu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onclick={handleClose} id="hot">hot</MenuItem>
+                            <MenuItem onclick={handleClose} id="new">new</MenuItem>
+                            <MenuItem onclick={handleClose} id="rising">rising</MenuItem>
+                            <MenuItem onclick={handleClose} id="controversial">controversial</MenuItem>
+                            <MenuItem onclick={handleClose} id="top">top</MenuItem>
+                        </Menu>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Create config
                     </Button>
-                </form>
-            </div>
-            <Box mt={8}>
+                    </form>
+                </div>
                 <Copyright />
-            </Box>
-        </Container>
+            </Container>
+        </div>
     )
 }
