2e0bb694277ac2e613b9094520f730bad00baba2
NAEK2K <naek2k@outlook.com>
2021-01-30 15:28:44 -0500

    scrapr supports different sorting

--- scrapr.py
+++ scrapr.py
@@ -22,6 +22,9 @@ class Scrapr:
     def get_config(self):
         return self.config
 
+    def get_db(self):
+        return sqlite3.connect(self.db_path)
+
 
 class RedditScrapr(Scrapr):
     def __init__(self, config, praw_config):
@@ -35,7 +38,7 @@ class RedditScrapr(Scrapr):
 
     def init_db(self):
         if not os.path.exists(self.db_path):
-            conn = sqlite3.connect(self.db_path)
+            conn = super().get_db()
             c = conn.cursor()
             c.execute(
                 "CREATE TABLE IF NOT EXISTS reddit_scrapr (permalink text PRIMARY KEY, title text, selftext text, author text, keywords text);"
@@ -43,7 +46,7 @@ class RedditScrapr(Scrapr):
             conn.commit()
 
     def insert_submission(self, permalink, title, selftext, author, keywords):
-        conn = sqlite3.connect(self.db_path)
+        conn = super().get_db()
         c = conn.cursor()
         c.execute(
             "SELECT permalink FROM reddit_scrapr WHERE permalink = ?", [permalink]
@@ -77,16 +80,55 @@ class RedditScrapr(Scrapr):
         if text_regex.search(combined_text):
             self.insert_submission(permalink, title, selftext, author, keywords_regex)
 
-    def scrape_hot(self):
+    def scrape(self):
         sorting = self.config.get("sorting")
+        if sorting == "hot":
+            self.scrape_hot()
+        if sorting == "new":
+            self.scrape_new()
+        if sorting == "top":
+            self.scrape_top()
+        if sorting == "rising":
+            self.scrape_rising()
+        if sorting == "controversial":
+            self.scrape_controversial()
+
+    def scrape_hot(self):
         limit = self.config.get("limit")
         for submission in self.praw.subreddit(self.config.get("subreddit")).hot(
             limit=limit
         ):
-            print("Scraping: {}".format(submission.title))
+            self.scrape_submission(submission)
+
+    def scrape_new(self):
+        limit = self.config.get("limit")
+        for submission in self.praw.subreddit(self.config.get("subreddit")).new(
+            limit=limit
+        ):
+            self.scrape_submission(submission)
+
+    def scrape_top(self):
+        limit = self.config.get("limit")
+        for submission in self.praw.subreddit(self.config.get("subreddit")).top(
+            limit=limit
+        ):
+            self.scrape_submission(submission)
+
+    def scrape_rising(self):
+        limit = self.config.get("limit")
+        for submission in self.praw.subreddit(self.config.get("subreddit")).rising(
+            limit=limit
+        ):
+            self.scrape_submission(submission)
+
+    def scrape_controversial(self):
+        limit = self.config.get("limit")
+        for submission in self.praw.subreddit(
+            self.config.get("subreddit")
+        ).controversial(limit=limit):
             self.scrape_submission(submission)
 
 
 if __name__ == "__main__":
     scrapr = RedditScrapr("config.json", "praw_config.json")
-    scrapr.scrape_hot()
+    scrapr.scrape()
