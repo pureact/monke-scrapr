5c11215a646beeef8d62aa7d810bb58d6c71a295
NAEK2K <naek2k@outlook.com>
2021-01-30 18:03:33 -0500

    automatic generation of db name

--- backend/util.py
+++ backend/util.py
@@ -5,6 +5,10 @@ from uuid import uuid4
 
 def generate_config(config_name, directory=".", **params):
     file_path = "{}/{}_{}.json".format(directory, uuid4().hex, config_name)
+    params.update({"db_name": uuid4().hex})
     with open(file_path, "w+") as f:
         json.dump(params, f)
     return file_path
+
+
+generate_config("test", reddit_type="my_config")
