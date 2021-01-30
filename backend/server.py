536b93598f66de0ec82bbdb39f6add307ffd8454
Hayden Mirza <haydenjwmirza@gmail.com>
2021-01-30 13:36:58 -0500

    Initial routing

--- backend/server.py
+++ backend/server.py
@@ -1,2 +1,24 @@
+#Backend API and routing for monkeScrapr
+#By:
+#   Hayden Mirza
+#   Kyle SP
+
 import sqlite3
-conn = sqlite3.connect('users.db')
+from flask import Flask
+
+userConn = sqlite3.connect('users.db')
+app = Flask(__name__)
+
+#Default landing page
+@app.route('/')
+def index():
+    return 'Hello world'
+
+#Register user
+
+#Login user
+
+#Logout user
+
+
+app.run(debug=True, host='0.0.0.0')
